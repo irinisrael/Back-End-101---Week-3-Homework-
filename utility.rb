01607cfa3c1ed73147a731db4d235697619dc265
Irin Israel <irinisrael@icloud.com>
2015-12-09 11:22:40 -0500

    utility works, but comma issue

--- utility.rb
+++ utility.rb
@@ -1,2 +1,14 @@
-class
-	
+class Utility
+	def count_words(sentence)
+		names = sentence.split()
+		counts = Hash.new(0)
+		names.each { |name| counts[name] += 1}
+		puts counts
+	end
+end
+
+utility = Utility.new
+
+utility.count_words("the quick and the dead")
+#{"the" => 2, "quick" => 1, "and" => 1, "dead" => 1}
+utility.count_words("it was the best of times, it was the worst of times")
