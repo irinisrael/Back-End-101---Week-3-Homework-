e71092f2faa8c30a4ae3b9cb19076c4ae04b8f46
Irin Israel <irinisrael@icloud.com>
2015-12-08 21:07:01 -0500

    initial commit

---
+++ questions.rb
@@ -0,0 +1,7 @@
+# What is the difference between an HTTP GET request vs a POST request?
+
+# In GET, you ask to given info about the resource indicated by this url.
+# GET requests data from a specified source.
+
+# In POST, you try to make a change related to this resource and you usually make a new resource.
+# POST submits data to be processed to a specified source.
