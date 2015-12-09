b3047baf016262d450e65981d33f5d940ebdbdf6
Irin Israel <irinisrael@icloud.com>
2015-12-08 21:22:45 -0500

    finished questions and rails

--- homework3-project/config/routes.rb
+++ homework3-project/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get 'homework3' => 'home#index'
+  get 'silly' => 'home#silly'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
