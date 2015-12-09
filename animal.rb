a2ae220cf2420231f08ce8b8b57cd69ba7bd7c17
Irin Israel <irinisrael@icloud.com>
2015-12-09 10:06:34 -0500

    animal class done

--- animal.rb
+++ animal.rb
@@ -1,2 +1,18 @@
-class
+class Animal
+	def speak
+		puts "ROAR!!!"
+	end
+end
 	
+class Dog < Animal
+	def speak
+		puts "Actually, excuse me, I bark. RUFF!"
+	end
+end
+
+
+animal = Animal.new
+dog = Dog.new
+
+animal.speak
+dog.speak
